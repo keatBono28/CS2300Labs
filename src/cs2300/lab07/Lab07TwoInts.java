6bcc75b51cfdc0a4dda4eeceaf120114a6746247
kybon <kybon@192.168.0.19>
2019-02-27 09:25:44 -0500

    lab 07 finished

--- src/cs2300/lab07/Lab07TwoInts.java
+++ src/cs2300/lab07/Lab07TwoInts.java
@@ -13,13 +13,56 @@ package cs2300.lab07;
  */
 public class Lab07TwoInts {
     
-    private final int a = 0;
-    private final int b = 0;
+    private static int a = 0;
+    private static int b = 0;
     
+    Lab07TwoInts(int a, int b){
+        this.a = a;
+        this.b = b;
+        
+    }
     
-    
+    // To String Method
+    public String toString(){
+        String strToReturn = "<" + a + ", " + b + ">";
+        return strToReturn; 
+    }
+    public int getA(){
+        return a;
+    }
+    public int getB(){
+        return b;
+    }
+    public boolean isAPos(){
+        if(a > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean isBPos(){
+        if(b > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
     
     public static void main(String[] args) {
-          
+        // First Example : Blue
+        Lab07TwoInts blue = new Lab07TwoInts(4,11);
+        System.out.println("blue = " + blue);
+        // Second Example : Green
+        Lab07TwoInts green = new Lab07TwoInts(14,-1);
+        System.out.println("green = " + green);
+        System.out.println("blue.b = " + blue.getB());
+        // Logic for pos or neg
+        if (green.isAPos()) {
+            System.out.println("Yep!");
+        }else{
+            System.out.println("Nope!");
+        }
+        
+        
     }
 }
