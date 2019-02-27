05edf76c9eee27aacc4a8ddb800a961ddc203f5a
Keaton Bonomo <kybonomo@hotmail.com>
2019-02-27 07:51:25 -0500

    Start of Lab 07

---
+++ src/cs2300/lab07/Lab07TwoInts.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs2300.lab07;
+
+/**
+ * @author  Keaton Bonomo
+ * Ohio ID: kb619814
+ * Lab #7
+ * 2/27/2019
+ */
+public class Lab07TwoInts {
+    
+    private final int a = 0;
+    private final int b = 0;
+    
+    
+    
+    
+    public static void main(String[] args) {
+          
+    }
+}
